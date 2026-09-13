2eb452f7133ffb36d4cc3ac4630b79d572da3dac
sampaiosanders71-ai <sampaiosanders71@gmail.com>
2026-09-13 14:06:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'family-finance-';
-const CACHE_NAME = 'family-finance-cache-2026-09-11-secure-write-cors-fix';
+const CACHE_NAME = 'family-finance-cache-2026-09-13-rebuild-stage-7-8-9';
 const CORE_ASSETS = [
   './',
   './index.html',
