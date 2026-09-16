3d43520a5970662a9051abefec42ca60c4c5044c
sampaiosanders71-ai <sampaiosanders71@gmail.com>
2026-09-15 23:35:11 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://wggxeqfkxpwdefpzubyc.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_lNCWjemhH5_t8PReB8pvbQ_RNVl1bfj';
-export const APP_ENV = 'rebuild-test';
