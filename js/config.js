bbaabae3ad7dddb6a36d45c1f5b8ba5254196293
sampaiosanders71-ai <sampaiosanders71@gmail.com>
2026-09-15 23:43:03 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://wggxeqfkxpwdefpzubyc.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_lNCWjemhH5_t8PReB8pvbQ_RNVl1bfj';
+export const APP_ENV = 'rebuild-test';
